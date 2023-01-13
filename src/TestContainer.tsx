e48a6348bae63ad8ffebd7df7901cfa3ad59aaef
Justin <justin@marsnvenus.net>
2023-01-13 16:51:55 +0900

    css

--- src/TestContainer.tsx
+++ src/TestContainer.tsx
@@ -7,15 +7,8 @@ import ProgressBar from "./general/components/ProgressBar";
 import "./containerStyles.scss";
 
 export default function TestContainer() {
-  const [aniToggle, setAniToggle] = useState<Boolean>();
+  const [aniToggle, setAniToggle] = useState<Boolean>(); // animation toggle for all moving / animated components
   const [completed, setCompleted] = useState<Number>(10);
-  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
-  const handleClick = () => {
-    setCompleted(completed.valueOf() + 30);
-    // console.log(completed);
-    // console.log(aniToggle);
-  };
 
   // awaits 1 second to activate animation, then turns it off
   useEffect(() => {
@@ -30,7 +23,11 @@ export default function TestContainer() {
       <div className="test-container-header">
         <h4>Header</h4>
         <ProgressBar completed={completed} toggle={aniToggle}></ProgressBar>
-        <button onClick={() => handleClick()}>add</button>
+
+        {/* Buttons for testing */}
+        <button onClick={() => setCompleted(completed.valueOf() + 30)}>
+          add
+        </button>
         <button onClick={() => setCompleted(10)}>reset</button>
       </div>
     </div>
--- src/general/components/ProgressBar.tsx
+++ src/general/components/ProgressBar.tsx
@@ -1,28 +1,29 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 // css
 import "./styling/progressBarStyling.scss";
 
 export default function ProgressBar(props: any) {
   const { completed, toggle } = props;
-  const [animate, setAnimate] = useState<Number>(0);
-  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  const [animate, setAnimate] = useState<Boolean>(false);
 
+  // eslint-disable-lines
   useEffect(() => {
     renderAnimation();
   }, [toggle]);
 
+  // fills invisible width left of 'diamond' - pushes right
   const widthStyle = {
     width: `${completed}%`,
   };
 
+  // triggers animation keyframe
   const diaStyle = {
     animation: animate ? "spin 1s ease-in-out" : "none",
-    // animateionDuration: animate ? "500ms" : 0,
   };
 
   const renderAnimation = () => {
-    return toggle ? setAnimate(1) : setAnimate(0);
+    return toggle ? setAnimate(true) : setAnimate(false);
   };
 
   return (
