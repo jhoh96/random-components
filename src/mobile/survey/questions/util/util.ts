b7823dbd3aa18e3cece7afc75780bc78ecbc278e
Justin <dearlybeloved0214@gmail.com>
2023-03-15 15:16:45 +0900

    stuff

--- src/mobile/survey/questions/util/util.ts
+++ src/mobile/survey/questions/util/util.ts
@@ -13,18 +13,27 @@ export const saveQuestion = (question: Question) => {
   // TODO : POST to db
 };
 
-
 // if question Type === single
 // only set one answer obj to true
-export const checkAnswer = (answers:any[], answerType:string) => {
-  if(answerType === 'multiple') {
+export const checkAnswer = (answers: any[], answerType: string) => {
+  if (answerType === "multiple") {
+    console.log("its multiple choice");
     return false;
   } else {
-    for(const [key, value] of Object.entries(answers)) {
-      if(value === true) {
+    for (const [key, value] of Object.entries(answers)) {
+      if (value === true) {
         let index = answers.indexOf([key,value])
-        console.log(index)
+        // answers.indexOf(index) = 
+        return true;
       }
     }
   }
 };
+
+export function ensure<T>(argument: T | undefined | null, message: string = 'This value was promised to be there.'): T {
+  if (argument === undefined || argument === null) {
+    throw new TypeError(message);
+  }
+
+  return argument;
+}
